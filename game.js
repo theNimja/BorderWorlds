dac5f8c46c2d10d95b67d63512b58605e2668c7a
nimrod libman <nimrodlibman@yahoo.c.uk>
2014-07-28 13:36:26 +0100

    Comments
    Mostly comments.also, A line was moved for better practise.

--- game.js
+++ game.js
@@ -1,12 +1,14 @@
+//Set up globs
+//Ze important information goes HERE!
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 var waypointX;
 var waypointY;
-
+//Set up images
 imgShip = new Image();
 imgShip.src = "images/protoShip.png";
 imgShip.addEventListener("load", init, false);
-
+//banana banana banana
 var requestAnimFrame=
 	window.requestAnimationFrame ||
 	window.webkitRequestAnimationFrame ||
@@ -16,33 +18,37 @@ var requestAnimFrame=
 	function(callback){
 	window.setTimeout(callback,1000/60);
 	};
+	
+//important information on the ship	
 var shipX = 65;
 var shipY = 65;
+//banana banana banana
 function init(){
 	requestAnimFrame(update);
 }
-
+//drawing the frame.Most game code goes here.
 function update(){
 	context.clearRect(0,0,canvas.width,canvas.height)
-	context.drawImage(imgShip,shipX,shipY);
+	
 	requestAnimFrame(update);
-	console.log(waypointX+","+waypointY);
+	//move the ship
 	if (waypointX != shipX || waypointY != shipY){
 	if (waypointX < shipX){shipX--;}
 	if (waypointX > shipX){shipX++;}
 	if (waypointY < shipY){shipY--;}
 	if (waypointY > shipY){shipY++;}
 	}
-	
+	//draw the ship
+	context.drawImage(imgShip,shipX,shipY);
 }
-
+//getting info n mouse
 canvas.onmousedown = function(e){
 
     waypointX=getMousePos(canvas,e).x;
     waypointY=getMousePos(canvas,e).y;
 
 }
-
+//moar info on mouse
 function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect();
     return {
