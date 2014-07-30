f19c2d091879cce5e93f443b5a6bd05761eca9d4
Ross Kelso <rosskelso@btinternet.com>
2014-07-30 17:06:57 +0100

    Moreprettying

--- game/game.js
+++ game/game.js
@@ -285,6 +285,6 @@ function loadStore(pId) {
 		html = html + "<tr><td>" + cNames[i] + "</td><td  class='imageContainer'><a href='#' onclick='buy(" + i + "," + pId + "," + "1)'>" + getPrice(i, pId, true) + 
 			"</a></td><td class='imageContainer'><a href='#' onclick='sell(" + i + "," + pId + "," + "1)'>" + getPrice(i, pId, false) +"</a></td></tr>";
 	}
-	html = html + "<tr><td class='imageContainer'><a href='#' onclick='repair()'>Repair</a></td><td><a href='#' onclick='overlay()'>Close</a></td></tr></table>";
+	html = html + "<tr><td class='imageContainer'><a href='#' onclick='repair()'>Repair</a></td><td class='imageContainer'><a href='#' onclick='overlay()'>Close</a></td></tr></table>";
 	document.getElementById("store").innerHTML = html;
 }
